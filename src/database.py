2718df2437cc6951b0e61112fc1d635e1c8bb20e
luanfred <luanmatheussf4@gmail.com>
2025-09-16 01:21:19 -0300

    refactor: removing direct database url and taking it from .env

--- src/database.py
+++ src/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://user_admin:user_admin123@db:5432/cars_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
